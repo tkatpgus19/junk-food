0fb4e6b4aa5331f58a5fd87b019bf3649613080d
sehyun <germs1020@gmail.com>
2026-04-15 00:29:11 +0900

    init BE files

---
+++ BE/src/main/java/com/example/junk_food/JunkFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.junk_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunkFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunkFoodApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/example/junk_food/JunkFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junk_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunkFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
